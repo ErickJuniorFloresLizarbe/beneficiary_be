633bd0845616da5897fb19846a62cffd6f042818
junniordeveloper <erick.flores@vallegrande.edu.pe>
2024-12-05 12:50:17 -0500

    delete

--- src/main/java/pe/edu/vallegrande/monitoreo/repository/EducationRepository.java
+++
@@ -1,8 +0,0 @@
-// EducationRepository.java
-package pe.edu.vallegrande.monitoreo.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import pe.edu.vallegrande.monitoreo.model.Education;
-
-public interface EducationRepository extends ReactiveCrudRepository<Education, Integer> {
-}
--- src/main/java/pe/edu/vallegrande/monitoreo/repository/HealthRepository.java
+++
@@ -1,8 +0,0 @@
-// HealthRepository.java
-package pe.edu.vallegrande.monitoreo.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import pe.edu.vallegrande.monitoreo.model.Health;
-
-public interface HealthRepository extends ReactiveCrudRepository<Health, Integer> {
-}
